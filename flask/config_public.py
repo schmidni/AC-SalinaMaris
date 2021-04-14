2fc2e1e5472a26e5c250f067e3e7d184ae536b0d
Nicolas Schmid <nicolas@breiten.ch>
2021-04-14 09:15:54 +0200

    chore: add public config blueprint

--- flask/config_public.py
+++ flask/config_public.py
@@ -3,5 +3,12 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get(
-        'SECRET_KEY') or 'EC9F71CFE9D117F9EFB21EA96F88D'
+        'SECRET_KEY') or 'ASDFSASFDSDDSSWWERASD'
     SERVER_NAME = 'localhost:5000'
+    AC_KEY = "***"
+    AC_URL = "https://***.api-us1.com/api/3/"
+    STRIPE_PUBLIC_KEY = "pk_test_***"
+    STRIPE_SECRET_KEY = "sk_test_***"
+    SENDGRID_API_KEY = "SG.***"
+    SENDER_MAIL = "order@salina.maris.ch"
+    INTERNAL_MAIL = "info@salina.maris.ch"
